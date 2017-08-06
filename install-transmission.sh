79e02c7abbf360af026e19d2d94887ddcb2b4cc2
Oscar <oscarssama@users.noreply.github.com>
2017-08-05 22:42:39 -0500

    Update install-transmission.sh

--- install-transmission.sh
+++ install-transmission.sh
@@ -16,24 +16,24 @@ chown -R $uname.$uname /home/$uname/Downloads/
 chmod g+w /home/$uname/Downloads/
 
 # Install the firewall (CSF)
-cd /usr/local/src
-wget http://configserver.com/free/csf.tgz
-tar xzf csf.tgz
-cd csf
-./install.generic.sh
-cd /etc/csf
-sed -i 's/^TESTING =.*/TESTING = "0"/' csf.conf
-sed -i 's/^TCP_IN =.*/TCP_IN = "22,80,9091,10000,51413"/' csf.conf
-sed -i 's/^TCP_OUT =.*/TCP_OUT = "1:65535"/' csf.conf
-sed -i 's/^UDP_IN =.*/UDP_IN = "51413"/' csf.conf
-sed -i 's/^UDP_OUT =.*/UDP_OUT = "1:65535"/' csf.conf
-csf -r
+#cd /usr/local/src
+#wget http://configserver.com/free/csf.tgz
+#tar xzf csf.tgz
+#cd csf
+#./install.generic.sh
+#cd /etc/csf
+#sed -i 's/^TESTING =.*/TESTING = "0"/' csf.conf
+#sed -i 's/^TCP_IN =.*/TCP_IN = "22,80,9091,10000,51413"/' csf.conf
+#sed -i 's/^TCP_OUT =.*/TCP_OUT = "1:65535"/' csf.conf
+#sed -i 's/^UDP_IN =.*/UDP_IN = "51413"/' csf.conf
+#sed -i 's/^UDP_OUT =.*/UDP_OUT = "1:65535"/' csf.conf
+#csf -r
 
 # Install libevent
 cd /usr/local/src
-wget https://github.com/libevent/libevent/releases/download/release-2.0.22-stable/libevent-2.0.22-stable.tar.gz
-tar xzf libevent-2.0.22-stable.tar.gz
-cd libevent-2.0.22-stable
+wget https://github.com/libevent/libevent/releases/download/release-2.1.8-stable/libevent-2.1.8-stable.tar.gz
+tar xzf libevent-2.1.8-stable.tar.gz
+cd libevent-2.1.8-stable
 ./configure --prefix=/usr
 make
 make install
@@ -46,9 +46,9 @@ export PKG_CONFIG_PATH=/usr/lib/pkgconfig
 
 # Install transmission
 cd /usr/local/src
-wget https://transmission.cachefly.net/transmission-2.84.tar.xz
-tar xvf transmission-2.84.tar.xz
-cd transmission-2.84
+wget https://transmission.cachefly.net/transmission-2.92.tar.xz
+tar xvf transmission-2.92.tar.xz
+cd transmission-2.92
 ./configure --prefix=/usr
 make
 make install
